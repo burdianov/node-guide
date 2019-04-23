1436de996a2aa1cb256df4dccc69b0aa4af36ef6
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-04-23 22:46:43 +0400

    basic requests

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
 });
 
 server.listen(3000, () => {
   console.log("server running...");
-  process.exit();
 });
