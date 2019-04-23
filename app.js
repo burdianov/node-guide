b013fbb460329ec9b085a561f22eb6046bf1ee27
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-04-23 22:57:32 +0400

    send response

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<html>");
+  res.write("<head><title>Test page</title></head>");
+  res.write("<body><h1>Test response</h1></body>");
+  res.write("</html");
+  res.end();
 });
 
 server.listen(3000, () => {
