a899383938463dbf148169301860be45273fab5d
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-04-24 22:32:11 +0400

    parse request body

--- app.js
+++ app.js
@@ -9,14 +9,24 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
     res.write(
-      "<body><form action='/message' method='POST' message='testMessage'><input type='text' name='message'/><button type='submit'>Send</button></form></body>"
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>"
     );
     res.write("</html");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "dummy");
+    const body = [];
+    req.on("data", chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
