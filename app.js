066b2d66e7daa243c22a7e609235246a2050243d
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-04-26 22:46:47 +0400

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const http = require("http");
+const bodyParser = require("body-parser");
 
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>The Add Product page</h1>");
+  res.send(
+    "<form action='product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
